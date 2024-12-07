28aa92c7c0c86ab7a6874bccaab52e9a8a9e3ddd
Lore <3032882800108@ingenieria.usac.edu.gt>
2024-12-06 18:08:41 -0600

    Inicio de pagina web

---
+++ fase1-pages/src/routes/about/+page.svelte
@@ -0,0 +1,50 @@
+<script>
+
+</script>
+
+<div class="p-8 bg-gray-100 min-h-screen">
+<!-- text-4xl font-bold text-blue-600   text-xl font-medium text-gray-700 mt-2 -->
+<div class="text-center mb-8">
+  <h1 class="text-2xl font-medium text-gray-700 mt-2">Universidad de San Carlos de Guatemala</h1>
+  <h2 class="text-2xl font-medium text-gray-700 mt-2">Facultad de Ingenieria</h2>
+  <h3 class="text-2xl font-medium text-gray-700 mt-2">Ingenieria en Ciencias y Sistemas</h3>
+  <h4 class="text-2xl font-medium text-gray-700 mt-2">Organización de Lenguajes y Compiladores 2</h4>
+</div>
+
+<div class="bg-white p-6 rounded-lg shadow-lg">
+  <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+    <!-- Integrante 1 -->
+    <div class="flex flex-col items-center bg-gray-50 p-4 rounded-lg shadow-md">
+      <div class="w-24 h-24 flex items-center justify-center bg-blue-100 rounded-full mb-4">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-16 w-16 text-blue-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A10.97 10.97 0 0112 15c2.89 0 5.527 1.133 7.373 2.987M15 11a4 4 0 11-8 0 4 4 0 018 0z" />
+        </svg>
+      </div>
+      <h4 class="text-lg font-semibold text-gray-800">Selvin Hernandez</h4>
+      <p class="text-gray-600 text-sm">Rol: Developer</p>
+    </div>
+
+    <!-- Integrante 2 -->
+    <div class="flex flex-col items-center bg-gray-50 p-4 rounded-lg shadow-md">
+      <div class="w-24 h-24 flex items-center justify-center bg-blue-100 rounded-full mb-4">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-16 w-16 text-blue-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A10.97 10.97 0 0112 15c2.89 0 5.527 1.133 7.373 2.987M15 11a4 4 0 11-8 0 4 4 0 018 0z" />
+        </svg>
+      </div>
+      <h4 class="text-lg font-semibold text-gray-800">Elder Lopez</h4>
+      <p class="text-gray-600 text-sm">Rol: Developer</p>
+    </div>
+
+    <!-- Integrante 3 -->
+    <div class="flex flex-col items-center bg-gray-50 p-4 rounded-lg shadow-md">
+      <div class="w-24 h-24 flex items-center justify-center bg-blue-100 rounded-full mb-4">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-16 w-16 text-blue-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A10.97 10.97 0 0112 15c2.89 0 5.527 1.133 7.373 2.987M15 11a4 4 0 11-8 0 4 4 0 018 0z" />
+        </svg>
+      </div>
+      <h4 class="text-lg font-semibold text-gray-800">Ingrid Lorena Garcia Yantuche</h4>
+      <p class="text-gray-600 text-sm">Rol: Developer</p>
+    </div>
+  </div>
+</div>
+</div>
